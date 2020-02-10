e0e01d3a97f99c833c30b27e80d285455605e2b5
Andrea <andrea.deidda@studenit.unipd.it>
2020-02-10 20:30:21 +0100

    Sistemato file System.sql

--- Fantacalcio/System.sql
+++ Fantacalcio/System.sql
@@ -2,7 +2,6 @@
 
 SET FOREIGN_KEY_CHECKS=0;
 
-DROP TABLE IF EXISTS File;
 DROP TABLE IF EXISTS Giocatore;
 DROP TABLE IF EXISTS Giornata;
 DROP TABLE IF EXISTS Classifica;
@@ -122,76 +121,6 @@ LOAD DATA LOCAL INFILE 'Squadra.txt' INTO TABLE Squadra;
 LOAD DATA LOCAL INFILE 'Fantallenatori.txt' INTO TABLE Fantallenatore;
 LOAD DATA LOCAL INFILE 'Classifica.txt' INTO TABLE Classifica;
 LOAD DATA LOCAL INFILE 'Giocatori.txt' INTO TABLE Giocatore;
+LOAD DATA LOCAL INFILE 'Partita.txt' INTO TABLE Partita;
 
 SET FOREIGN_KEY_CHECKS=1;
-
-
-
-
-
-CREATE TABLE IF NOT EXISTS Fantallenatore(
-        Nome_Utente varchar(20) NOT NULL PRIMARY KEY,
-        Password varchar(10) NOT NULL,
-        Budget_Rimanente int NOT NULL
-)ENGINE=InnoDB;
-
-
-
-
-
-CREATE TABLE IF NOT EXISTS Partita(
-NGiornata int REFERENCES Giornata (Numero_Giornata) ON DELETE CASCADE,
-NPartita int,
-Squadra_Casa varchar(20) REFERENCES Squadra (Nome) ON DELETE CASCADE,
-Squadra_Ospite varchar(20)REFERENCES Squadra (Nome) ON DELETE CASCADE,
-Gol_Casa int,
-Punteggio_Casa double,
-Gol_Ospite int,
-Punteggio_Ospite double,
-Punti_ClassificaC int,
-Punti_ClassificaO int,
-PRIMARY KEY(Squadra_Casa,Squadra_Ospite,NGiornata)
-)ENGINE=InnoDB;
-
-
-
-
-CREATE TABLE IF NOT EXISTS Voti_Giornata(
-ID_Giocatore varchar(10) REFERENCES Giocatore (ID) ON DELETE CASCADE,
-Voto double,
-Gol int,
-Gol_Subiti int,
-Rigori_Parati int,
-Rigori_Sbagliati int,
-Ammonizioni int,
-Espulsioni int,
-Assist int,
-Fantavoto double,
-Presenza varchar(20) NOT NULL,
-PRIMARY KEY(ID_Giocatore)
-)ENGINE=InnoDB;
-
-
-
-
-CREATE TABLE IF NOT EXISTS Squadra(
-Nome varchar(20) NOT NULL,
-Fantallenatore varchar(20) NOT NULL,
-Descrizione varchar(60),
-Valore_Squadra int NOT NULL,
-Colore varchar(10) NOT NULL,
-PRIMARY KEY(Nome,Fantallenatore),
-FOREIGN KEY (Fantallenatore) REFERENCES Fantallenatore (Nome_Utente)
-)ENGINE=InnoDB;
-
-LOAD DATA LOCAL INFILE 'Giornata.txt' INTO TABLE Giornata;
-LOAD DATA LOCAL INFILE 'VotiGiornata.txt' INTO TABLE Voti_Giornata;
-LOAD DATA LOCAL INFILE 'Squadra.txt' INTO TABLE Squadra;
-LOAD DATA LOCAL INFILE 'Fantallenatori.txt' INTO TABLE Fantallenatore;
-LOAD DATA LOCAL INFILE 'Classifica.txt' INTO TABLE Classifica;
-LOAD DATA LOCAL INFILE 'Giocatori.txt' INTO TABLE Giocatore;
-
-
-
-SET FOREIGN_KEY_CHECKS=1;
-
